76130dee8e3b5699c8a5e96cfe45ec3f79798237
vgavrilenko <v.gavrilenko@bleecker.uk>
2018-07-16 14:46:29 +0300

    Add settings

---
+++ FileWatcher/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace FileWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutFileWatcher100ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(307, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(434, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 99);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(434, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 290);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(467, 225);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(941, 218);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(521, 28);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Path to Log Folder";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(524, 47);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(411, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(521, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Number of files to copy";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(524, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 83);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Path to Inbox Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Path to NewDocuments Folder";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 271);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NewDocuments Folder Log";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(485, 290);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(468, 225);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(485, 271);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Inbox Folder Log";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(965, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutFileWatcher100ToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutFileWatcher100ToolStripMenuItem
+            // 
+            this.aboutFileWatcher100ToolStripMenuItem.Name = "aboutFileWatcher100ToolStripMenuItem";
+            this.aboutFileWatcher100ToolStripMenuItem.Size = new System.Drawing.Size(202, 22);
+            this.aboutFileWatcher100ToolStripMenuItem.Text = "About File Watcher 1.0.0";
+            this.aboutFileWatcher100ToolStripMenuItem.Click += new System.EventHandler(this.aboutFileWatcher100ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 545);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "File watcher";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutFileWatcher100ToolStripMenuItem;
+    }
+}
+
---
+++ FileWatcher/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace FileWatcher
+{
+    public partial class Form1 : Form
+    {
+        string pathNewDocuments = null;
+        string pathInbox = null;
+        string pathLog = null;
+        string logFileName = "NewDocuments-log-" + DateTime.Now.ToString("yyyyMMddTHHmmss") + ".txt";
+        string logFileNameInbox = "Inbox-log-" + DateTime.Now.ToString("yyyyMMddTHHmmss") + ".txt";
+        int countCopy = 1;
+        int delayCopy = 300000;
+
+        bool taskOpen = false;
+        System.Threading.Timer timer;
+        FileSystemWatcher fs;
+        FileSystemWatcher fsInbox;
+        string textOneLineLog = "";
+        string textOneLineLogInbox = "";
+        string fullPath = "";
+        string fullPathInbox;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.pathNewDocuments = ReadSetting("pathNewDocuments");
+            this.pathInbox = ReadSetting("pathInbox");
+            this.pathLog = ReadSetting("pathLog");
+            this.countCopy = Convert.ToInt32(ReadSetting("countCopy"));
+            this.delayCopy = Convert.ToInt32(ReadSetting("delayCopy"));
+
+            textBox2.Text = this.pathNewDocuments;
+            textBox3.Text = this.pathInbox;
+            textBox1.Text = this.countCopy.ToString();
+            textBox4.Text = this.pathLog;
+
+            this.fullPath = this.pathLog + "/" + this.logFileName;
+            this.fullPathInbox = this.pathLog + "/" + this.logFileNameInbox;
+            RunWatch();
+        }
+
+        public void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                MessageBox.Show("Error writing app settings");
+            }
+        }
+
+        public string ReadSetting(string key)
+        {
+            string result = "";
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                result = appSettings[key] ?? "Not Found";
+            }
+            catch (ConfigurationErrorsException)
+            {
+                MessageBox.Show("Error reading app settings");
+            }
+            return result;
+        }
+
+        private void RunWatch()
+        {
+            //FileInfo fileInf = new FileInfo(pathFile);
+            //fileInf.Create();
+
+            if(fs == null)
+            {
+                fs = new FileSystemWatcher();
+                fs.Path = this.pathNewDocuments;
+                fs.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                //fs.Filter = "*.pdf";
+                fs.Changed += new FileSystemEventHandler(onChange);
+                fs.Created += new FileSystemEventHandler(onChange);
+                fs.Deleted += new FileSystemEventHandler(onChange);
+                fs.Renamed += new RenamedEventHandler(onChange);
+                fs.EnableRaisingEvents = true;
+            }
+
+            if (fsInbox == null)
+            {
+                fsInbox = new FileSystemWatcher();
+                fsInbox.Path = this.pathInbox;
+                fsInbox.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+                //fs.Filter = "*.pdf";
+                fsInbox.Created += new FileSystemEventHandler(onChangeInbox);
+                fsInbox.Deleted += new FileSystemEventHandler(onChangeInbox);
+                fsInbox.EnableRaisingEvents = true;
+            }
+
+            if (this.timer != null)
+            {
+                this.timer.Change(Timeout.Infinite, Timeout.Infinite);
+            }
+            // устанавливаем метод обратного вызова
+            TimerCallback tm = new TimerCallback(Count);
+            // создаем таймер
+            this.timer = new System.Threading.Timer(tm, null, 0, 500);
+
+            //fs.WaitForChanged(WatcherChangeTypes.All, 10000);
+
+            //FileSystemWatcher fs2 = new FileSystemWatcher();
+            //fs2.Path = this.pathSource;
+            //fs2.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            ////fs.Filter = "*.pdf";
+            //fs2.Changed += new FileSystemEventHandler(onChange);
+            //fs2.Created += new FileSystemEventHandler(onChange);
+            //fs2.Deleted += new FileSystemEventHandler(onChange);
+            //fs2.Renamed += new RenamedEventHandler(onChange);
+            //fs2.WaitForChanged(WatcherChangeTypes.All, 10000);
+            //fs2.EnableRaisingEvents = true;
+
+        }
+
+        private void onChangeInbox(object sender, FileSystemEventArgs e)
+        {
+            
+            textOneLineLogInbox = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + " --File: " + e.FullPath + " " + e.ChangeType + "\n\r";
+            richTextBox2.Invoke(new Action(() => richTextBox2.Text += textOneLineLogInbox));
+            richTextBox2.Invoke(new Action(() => richTextBox2.SaveFile(this.fullPathInbox, RichTextBoxStreamType.PlainText)));
+           
+        }
+
+        private void Count(object state)
+        {
+            if (isDirectoryEmpty() && !isDirectorySourceEmpty() && !taskOpen )
+            {
+                taskOpen = true;
+                setTimer();
+            }
+        }
+
+        private void onChange(object sender, FileSystemEventArgs e)
+        {
+            //FileSystemWatcher f = sender as FileSystemWatcher;
+            //MessageBox.Show(f.Path);
+            //richTextBox1.Text += DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + " --File: " + e.FullPath + " " + e.ChangeType + "\n\r";
+
+            textOneLineLog = DateTime.Now.ToShortDateString() + " " + DateTime.Now.ToLongTimeString() + " --File: " + e.FullPath + " " + e.ChangeType + "\n\r";
+            richTextBox1.Invoke(new Action(() => richTextBox1.Text += textOneLineLog));
+            richTextBox1.Invoke(new Action(() => richTextBox1.SaveFile(this.fullPath, RichTextBoxStreamType.PlainText)));
+            //richTextBox1.Text = textOneLineLog;
+            //richTextBox1.SaveFile(this.logFileName, RichTextBoxStreamType.PlainText);
+
+            //if (isDirectoryEmpty())
+            //{
+            //    setTimer();
+            //}
+        }
+
+        private async void setTimer()
+        {
+            await Task.Delay(this.delayCopy);
+            fileMove();
+            taskOpen = false;
+        }
+
+        private void fileMove()
+        {
+            DirectoryInfo dir = new DirectoryInfo(this.pathInbox);
+            FileInfo[] files = dir.GetFiles();
+
+            int k = 0;
+            foreach (FileInfo f in files)
+            {
+                if (k == countCopy)
+                {
+                    break;
+                }
+                if (f.Length > 0)
+                {
+                    f.MoveTo(this.pathNewDocuments + "/" + f.Name);
+                }
+
+                k++;
+            }
+
+        }
+
+        private bool isDirectoryEmpty()
+        {
+            return !Directory.EnumerateFileSystemEntries(this.pathNewDocuments).Any();
+        }
+
+        private bool isDirectorySourceEmpty()
+        {
+            return !Directory.EnumerateFileSystemEntries(this.pathInbox).Any();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.pathNewDocuments = textBox2.Text;
+            this.pathInbox = textBox3.Text;
+            this.pathLog = textBox4.Text;
+            this.countCopy = Convert.ToInt32(textBox1.Text);
+
+            AddUpdateAppSettings("pathNewDocuments", this.pathNewDocuments);
+            AddUpdateAppSettings("pathInbox", this.pathInbox);
+            AddUpdateAppSettings("pathLog", this.pathLog);
+            AddUpdateAppSettings("countCopy", Convert.ToString(this.countCopy));
+
+            this.fullPath = this.pathLog + "/" + this.logFileName;
+            this.fullPathInbox = this.pathLog + "/" + this.logFileNameInbox;
+            RunWatch();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutFileWatcher100ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm form2 = new AboutForm();
+            form2.Show();
+        }
+    }
+}
