30f1598e850ab41e888e3e4f0ad967e10df6346e
vgavrilenko <v.gavrilenko@bleecker.uk>
2018-07-17 12:50:39 +0300

    Fix label

--- FileWatcher/Form1.Designer.cs
+++ FileWatcher/Form1.Designer.cs
@@ -219,9 +219,9 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(4, 131);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(104, 13);
+            this.label7.Size = new System.Drawing.Size(88, 13);
             this.label7.TabIndex = 12;
-            this.label7.Text = "Delay in milliseconds";
+            this.label7.Text = "Delay in seconds";
             // 
             // Form1
             // 
